0f4941059c3829f34e3a54b66c46b5d2cb84e456
F_fredes <felipe.fredes@ug.uchile.cl>
2018-06-15 22:33:12 -0300

    first commit again

---
+++ markov_chain_TCP.m
@@ -0,0 +1,33 @@
+clear
+p = 0.001;
+N = 1000; % iteraciones
+C = ceil(sqrt(3/(2*p)))*10; % estados (ventanas de congestion, cwnd)
+cwnd_min = 2;
+cwnd_max = C;
+prev_weight = zeros(1, C); % vector de los pesos (peso = probabilidad de que este en ese estado)
+prev_weight(cwnd_min) = 1;
+weight = zeros(1, C); % siguiente vector de peso
+
+error = 1e-6;
+while 1 
+    for cwnd = cwnd_min:cwnd_max
+        weight(min(cwnd_max,cwnd+1)) = weight(min(cwnd_max,cwnd+1)) + prev_weight(cwnd)*(1-p)^cwnd;
+        weight(max(cwnd_min, ceil(cwnd/2))) = weight(max(cwnd_min, ceil(cwnd/2))) + prev_weight(cwnd)*(1-(1-p)^cwnd);
+    end
+%     fprintf('weight = %1.16f\n', sum(weight)) % debería ser 1 siempre, si no es 1 se está "perdiendo" el peso
+    stem(weight)
+%     axis([25 43 0.022 0.027]) % prueba con p = 0.001
+    axis([0 ceil(sqrt(3/(2*p))/7)*20 0 max(weight)])
+%     fprintf('error = %4.12f\n', sum(abs(prev_weight-weight)))
+    pause(0.1)
+%     drawnow
+    if sum(abs(prev_weight-weight)) < error
+        break
+    end
+    prev_weight = weight;
+    weight = zeros(1, C);
+end
+
+average = sum(prev_weight.*(1:C));
+fprintf('theoretical = %4.3f, ', sqrt(3/(2*p)))
+fprintf('simulation = %4.3f\n', average)
